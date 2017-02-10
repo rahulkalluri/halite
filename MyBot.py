4d6802fc16f6d9569f5005a5425c1e3b3dd73414
Rahul Kalluri <rahkalluri@gmail.com>
2017-02-09 22:31:04 -0600

    increase utilization
    pieces will only move when production*factor(x) = current strength value

--- MyBot.py
+++ MyBot.py
@@ -1,16 +1,24 @@
 import hlt
 from hlt import NORTH, EAST, SOUTH, WEST, STILL, Move, Square
 import random
+from networking import *
+import sys
+import numpy as np
+
+
 
 
 myID, game_map = hlt.get_init()
 hlt.send_init("rahulkalluri")
 
 def zero_still(square):
-	if square.strength == 0:
+	if square.strength < 6 * square.production:
 		return Move(square, STILL)
 	else:
 		return Move(square, random.choice((NORTH, EAST, SOUTH, WEST, STILL)))
+
+		
+		
 while True:
     game_map.get_frame()
     moves = [Move(square, random.choice((NORTH, EAST, SOUTH, WEST, STILL))) for square in game_map if square.owner == myID]
