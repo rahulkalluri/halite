d369790fc720ee15109dc026dc484198d79b1287
Rahul Kalluri <rahkalluri@gmail.com>
2017-02-09 22:38:15 -0600

    improve attack and move pattern
    only attack if neighbor is weaker
    only move if it's worth the move (strength < production * value)

--- MyBot.py
+++ MyBot.py
@@ -12,14 +12,24 @@ myID, game_map = hlt.get_init()
 hlt.send_init("rahulkalluri")
 
 def zero_still(square):
+
+	#only attack if neighbor is weaker than our current piece
+	for direction, neighbor in enumerate(game_map.neighbors(square)):
+		if neighbor.owner != myID and neighbor.strength < square.strength:
+			return Move(square, direction)
+	
+	#only move if the strength is more than a certain value
 	if square.strength < 6 * square.production:
 		return Move(square, STILL)
+		
 	else:
-		return Move(square, random.choice((NORTH, EAST, SOUTH, WEST, STILL)))
+		#remove randomness of blocks moving, since that's inefficient
+		return Move(square, random.choice(SOUTH, EAST)))
 
 		
 		
 while True:
     game_map.get_frame()
-    moves = [Move(square, random.choice((NORTH, EAST, SOUTH, WEST, STILL))) for square in game_map if square.owner == myID]
+	
+    moves = [zero_still(square) for square in game_map if square.owner == myID]
     hlt.send_frame(moves)
